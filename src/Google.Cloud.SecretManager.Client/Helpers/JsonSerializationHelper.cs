ba330754fd59bea4858406d0b2a07e033a14e9cf
Dmitry Sigalov <dmitry.s@lsports.eu>
2025-01-13 14:30:44 +0200

    create-first-src-test-projects (#1)
    * empty solution
    
    * First set of helpers
    
    * setup workflow

---
+++ src/Google.Cloud.SecretManager.Client/Helpers/JsonSerializationHelper.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json;
+
+namespace Google.Cloud.SecretManager.Client.Helpers;
+
+public static class JsonSerializationHelper
+{
+    public static TObject Deserialize<TObject>(string source)
+    {
+        if (source == null)
+        {
+            return default(TObject);
+        }
+
+        return JsonConvert.DeserializeObject<TObject>(source);
+    }
+
+    public static string Serialize<TObject>(TObject source)
+    {
+        return JsonConvert.SerializeObject(source, Formatting.Indented);
+    }
+}
